eb55fc3f9cd8abcd8aa2b2d88a3c6c5f5bf71fd8
SalmanAlnajdi <salman.it095@gmail.com>
2021-01-04 03:00:24 +0300

    Note

---
+++ newtest/app/src/main/java/com/example/schoolapp/activities/MainPage.java
@@ -0,0 +1,53 @@
+package com.example.schoolapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.example.schoolapp.R;
+import com.example.schoolapp.adapter.FragmentAdp;
+import com.google.android.material.tabs.TabLayout;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+
+public class MainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+
+        ViewPager vp = findViewById(R.id.vp);
+        TabLayout tab = findViewById(R.id.tab);Toolbar toolbar = findViewById(R.id.toolbar);
+
+       PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName("Home").withIcon(R.drawable.ic_baseline_home_24);
+       PrimaryDrawerItem item2 = new PrimaryDrawerItem().withIdentifier(2).withName("History").withIcon(R.drawable.ic_baseline_history_edu_24);
+
+
+        Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .addDrawerItems(
+                        item1,
+                        new DividerDrawerItem(),
+                        item2
+                )
+                .build();
+
+
+        tab.setupWithViewPager(vp);
+
+
+        FragmentAdp adapter = new FragmentAdp(getSupportFragmentManager());
+        vp.setAdapter(adapter);
+
+        tab.getTabAt(0).setIcon(R.drawable.ic_baseline_home_24);
+        tab.getTabAt(1).setIcon(R.drawable.ic_baseline_class_24);
+        tab.getTabAt(2).setIcon(R.drawable.ic_baseline_fiber_new_24);
+
+    }
+}
